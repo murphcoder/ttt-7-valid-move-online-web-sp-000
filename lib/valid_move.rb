25fad549dc59c3e48db7fe2fb1786b67316c2e86
Sean Murphy <filmmakersean@fastmail.net>
2019-04-05 01:03:26 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(move)
-  move >= 0 && move <= 8
+  move >= 0 && move <= 8 && position_taken?
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
